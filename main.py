8f78b978131a806136cc101a727332b27dd93086
stfwn <s.wijnja@me.com>
2022-01-15 12:43:47 +0100

    Remove outdated TODO comment

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ pl.seed_everything(23333, workers=True)
 
 def main(args):
     # Init model & data
-    dm = {"fmnist": data_modules.FashionMNISTDataModule, "cifar100": data_modules.CIFAR100DataModule}[
-        args.dataset
-    ](
-        data_dir=args.data_dir, num_workers=12
-    )  # TODO: Add CIFAR100
+    dm = {
+        "fmnist": data_modules.FashionMNISTDataModule,
+        "cifar100": data_modules.CIFAR100DataModule,
+    }[args.dataset](data_dir=args.data_dir, num_workers=12)
     model = {"resnet20": models.ResNet20}[args.model](
-        num_channels=dm.num_channels, num_classes=dm.num_classes, epochs=args.epochs
+        num_channels=dm.num_channels,
+        num_classes=dm.num_classes,
+        epochs=args.epochs,
     )
 
     # Add transformations
@@ -36,6 +37,8 @@ if __name__ == "__main__":
     parser.add_argument("--data-dir", default="./data")
     parser.add_argument("-e", "--epochs", type=int, default=200)
     parser.add_argument("-m", "--model", required=True, choices=["resnet20"])
-    parser.add_argument("-d", "--dataset", choices=["fmnist", "cifar100"], required=True)
+    parser.add_argument(
+        "-d", "--dataset", choices=["fmnist", "cifar100"], required=True
+    )
     args = parser.parse_args()
     main(args)
