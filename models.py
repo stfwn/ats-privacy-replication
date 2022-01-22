3f10c8ca60082999aba48352d931de5d49532832
stfwn <s.wijnja@me.com>
2022-01-22 18:35:24 +0100

    Add ConvNet model (fix #26)

--- models.py
+++ models.py
@@ -7,33 +7,9 @@ from torchvision.models.resnet import BasicBlock
 import torchmetrics
 
 
-class ResNet20(LightningModule):
-    """This is a LightningModule version of JonasGeiping's ResNet
-    implementation [1], which itself is a copy of torchvision's ResNet [2] with
-    added arguments for the number of layers and their strides.
-
-    [1]: https://github.com/JonasGeiping/invertinggradients
-    [2]: https://pytorch.org/vision/stable/_modules/torchvision/models/resnet.html
-    """
-
-    _make_layer = ResNet._make_layer
-
-    def __init__(
-        self,
-        num_channels,
-        num_classes,
-        epochs,
-        learning_rate=0.1,
-        momentum=0.9,
-        weight_decay=5e-4,
-        gamma=0.1,
-        nesterov=True,
-        bugged_loss=True,
-        **kwargs,
-    ):
+class ModelBase(LightningModule):
+    def __init__(self):
         super().__init__()
-        self.save_hyperparameters()
-
         # Metrics
         self.train_acc = torchmetrics.Accuracy()
         self.val_acc = torchmetrics.Accuracy()
@@ -41,58 +17,6 @@ class ResNet20(LightningModule):
 
         self.optimizer = optim.SGD
         self.loss_function = F.cross_entropy
-
-        # Configure _make_layer :shrug:
-        self.base_width = 64
-        self.inplanes = 16
-        self._norm_layer = nn.BatchNorm2d
-        self.dilation = 1
-        self.groups = 1
-
-        # Create layers
-        width = 16
-        self.layers = torch.nn.ModuleList(
-            [
-                nn.Conv2d(
-                    num_channels,
-                    self.inplanes,
-                    kernel_size=3,
-                    stride=1,
-                    padding=1,
-                    bias=False,
-                ),
-                self._norm_layer(width),
-                nn.ReLU(inplace=True),
-                self._make_layer(BasicBlock, width, 3, stride=1, dilate=False),
-                self._make_layer(
-                    BasicBlock, width * 2, 3, stride=2, dilate=False
-                ),
-                self._make_layer(
-                    BasicBlock, width * 4, 3, stride=2, dilate=False
-                ),
-                nn.AdaptiveAvgPool2d((1, 1)),
-                nn.Flatten(),
-                nn.Linear(
-                    (width * 8) // 2 * BasicBlock.expansion,
-                    num_classes,
-                ),
-            ]
-        )
-
-        # Init weights
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight, mode="fan_out", nonlinearity="relu"
-                )
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
 
     def configure_optimizers(self):
         optimizer = self.optimizer(
@@ -143,3 +67,136 @@ class ResNet20(LightningModule):
         self.log("loss/test", test_loss)
         self.log("acc/test", self.test_acc, on_epoch=True)
         return test_loss
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class ResNet20(ModelBase):
+    """This is a LightningModule version of JonasGeiping's ResNet
+    implementation [1], which itself is a copy of torchvision's ResNet [2] with
+    added arguments for the number of layers and their strides.
+
+    [1]: https://github.com/JonasGeiping/invertinggradients
+    [2]: https://pytorch.org/vision/stable/_modules/torchvision/models/resnet.html
+    """
+
+    _make_layer = ResNet._make_layer
+
+    def __init__(
+        self,
+        num_channels,
+        num_classes,
+        epochs,
+        learning_rate=0.1,
+        momentum=0.9,
+        weight_decay=5e-4,
+        gamma=0.1,
+        nesterov=True,
+        bugged_loss=True,
+        **kwargs,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        # Configure _make_layer :shrug:
+        self.base_width = 64
+        self.inplanes = 16
+        self._norm_layer = nn.BatchNorm2d
+        self.dilation = 1
+        self.groups = 1
+
+        # Create layers
+        width = 16
+        self.layers = torch.nn.ModuleList(
+            [
+                nn.Conv2d(
+                    num_channels,
+                    self.inplanes,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=False,
+                ),
+                self._norm_layer(width),
+                nn.ReLU(inplace=True),
+                self._make_layer(BasicBlock, width, 3, stride=1, dilate=False),
+                self._make_layer(
+                    BasicBlock, width * 2, 3, stride=2, dilate=False
+                ),
+                self._make_layer(
+                    BasicBlock, width * 4, 3, stride=2, dilate=False
+                ),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(
+                    (width * 8) // 2 * BasicBlock.expansion,
+                    num_classes,
+                ),
+            ]
+        )
+
+        # Init weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_out", nonlinearity="relu"
+                )
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+class ConvNet(ModelBase):
+    def __init__(
+        self,
+        num_channels,
+        num_classes,
+        epochs,
+        width=16,
+        learning_rate=0.1,
+        momentum=0.9,
+        weight_decay=5e-4,
+        gamma=0.1,
+        nesterov=True,
+        bugged_loss=True,
+        **kwargs,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.layers = torch.nn.ModuleList(
+            [
+                torch.nn.Conv2d(num_channels, width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(width, 2 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(2 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(2 * width, 2 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(2 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(2 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(4 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(4 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(3),
+                torch.nn.Conv2d(4 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(4 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(4 * width, 4 * width, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(4 * width),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(3),
+                torch.nn.Flatten(),
+                torch.nn.Linear(36 * width, num_classes),
+            ]
+        )
